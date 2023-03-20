691977b21fab4c7e53511bf72ab50bcac7d1648c
Daniel-Charles-J <80055586+Daniel-Charles-J@users.noreply.github.com>
2023-03-20 21:49:16 +0530

    completed-js

---
+++ javaScript/javaScript.js
@@ -0,0 +1,37 @@
+const x = "gkk";
+let y = 6;
+var z = 6;
+
+
+console.log(x,y,z);
+let string = "Kings Engineering College";
+console.log(string);
+let array = ["apple","orange","blueberry","mango"];
+console.log(array);
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+console.log(array.length);
+for(let i=0;i<array.length;i++)
+{
+    console.log(array[i]);
+}
+for(let i=0;i<=10;i++)
+{
+    console.log(i);
+}
+arr=[1,2,3,4,5]
+array_1 = arr.map((x)=> x*x);
+{
+    console.log(array_1);
+}
+let a=array.filter((x)=>x ==="blueberry");
+{
+    console.log(a);
+}
+let b=array.some((x)=>x==="blueberry");
+{
+    console.log(b);
+}
+     
